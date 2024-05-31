ab8e2349e2ebf28f47d3504848d38c70b590613e
VIKAS KUMAR VERMA <vikassoftfix@gmail.com>
2024-05-31 12:33:56 +0530

    fix: some changes

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -6,24 +6,24 @@ export function errorHandler(err: Error, req: Request, res: Response, next: Next
       // Handle specific types of errors
       if (err.name === 'ValidationError') {
          // Handle Mongoose validation errors
-         return res.status(400).json({ error: err.message });
+         return res.status(400).json({ msg: err.message });
       }
 
       // Handle CastError specifically
       if (err instanceof mongoose.Error.CastError) {
          // Handle Mongoose CastError (e.g., invalid ObjectId)
-         return res.status(400).json({ error: 'Invalid ID format.' });
+         return res.status(400).json({ msg: 'Invalid ID format.' });
       }
 
       if (err.name === 'JsonWebTokenError') {
          // Handle JWT errors
-         return res.status(401).json({ error: 'Invalid token.' });
+         return res.status(401).json({ msg: 'Invalid token.' });
       }
 
       // Handle other types of errors
-      res.status(500).json({ error: 'Something went wrong.' });
+      res.status(500).json({ msg: 'Something went wrong.' });
    } catch (error) {
       console.error('Error handling error:', error);
-      res.status(500).json({ error: 'Internal Server Error.' });
+      res.status(500).json({ msg: 'Internal Server Error.' });
    }
 }
